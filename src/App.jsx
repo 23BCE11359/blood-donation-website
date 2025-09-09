b272ef692da4c9f6d1795c6486d4e1ecc7bc7a1b
23BCE11359 <naman.23bce11359@vitbhopal.ac.in>
2025-09-10 01:48:18 +0530

    Added footer to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'; // Import icons from react-icons
+import { FaBars, FaTimes } from 'react-icons/fa';
 import HomePage from './components/HomePage';
 import DonorForm from './components/DonorForm';
 import AboutUs from './components/AboutUs';
@@ -12,22 +12,17 @@ function App() {
     <Router>
       <nav className="bg-gray-800 p-4">
         <div className="flex justify-between items-center">
-          {/* Mobile Menu Button */}
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             className="text-white md:hidden"
           >
             {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
-
-          {/* Desktop Menu */}
           <ul className="hidden md:flex space-x-4 text-white">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/donate">Donate</Link></li>
             <li><Link to="/about">About Us</Link></li>
           </ul>
-
-          {/* Mobile Menu */}
           {isMenuOpen && (
             <ul className="absolute top-16 left-0 w-full bg-gray-800 text-white flex flex-col space-y-2 p-4 md:hidden">
               <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
@@ -42,6 +37,9 @@ function App() {
         <Route path="/donate" element={<DonorForm />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
+      <footer className="bg-gray-800 p-4 text-white text-center">
+        &copy; 2025 Blood Donation Hub. Developed by Naman Dhakad.
+      </footer>
     </Router>
   );
 }
